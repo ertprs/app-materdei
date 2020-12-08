7dad326473235968d9c530463d8d6795a87b7b41
Lucas Ventura <lucas.alves@mobiauto.com.br>
2020-12-08 10:06:57 -0300

    Add more about CORS

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,16 @@ import cors from 'cors'
 
 const app = express()
 
-app.use(cors({ origin: 'https://materdeicam.org.br/' }))
+app.use(cors({
+    origin: 'https://materdeicam.org.br/'
+}))
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'https://materdeicam.org.br/');
+    res.setHeader('Access-Control-Allow-Methods', 'POST');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    next();
+});
 app.use(express.json())
 app.use(router)
 
