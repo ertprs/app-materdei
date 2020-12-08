79e8d4bbfd67f227441e7105ce1269bf2d2d954e
Lucas Ventura <lucas.alves@mobiauto.com.br>
2020-12-08 10:18:15 -0300

    Fixing CORS

--- src/app.ts
+++ src/app.ts
@@ -5,15 +5,10 @@ import cors from 'cors'
 const app = express()
 
 app.use(cors({
-    origin: 'https://materdeicam.org.br/'
+    origin: 'https://materdeicam.org.br',
+    methods: 'POST',
+    optionsSuccessStatus: 201
 }))
-
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'https://materdeicam.org.br/');
-    res.setHeader('Access-Control-Allow-Methods', 'POST');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    next();
-});
 app.use(express.json())
 app.use(router)
 
