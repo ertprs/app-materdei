148eaeb3b6eef7a2ed600db3e9fc76b50c532124
Lucas Ventura <lucas.alves@mobiauto.com.br>
2020-12-08 09:25:39 -0300

    Add CORS

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import { router } from './routes'
+import cors from 'cors'
 
 const app = express()
 
+app.use(cors({ origin: 'https://materdeicam.org.br/' }))
 app.use(express.json())
 app.use(router)
 
